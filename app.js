9817a186c38d50b287e0fc1ea8f4db2aa15277fa
ryuno-su <ryu.h28goo922@gmail.com>
2024-08-25 00:47:33 +0900

    Add comment app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// モジュールの読み込み
+var createError = require('http-errors');  // httpエラーの対処を行うためのもの
+var express = require('express');  // expressそのもの
+var path = require('path');  // ファイルパスを扱うためのもの
+var cookieParser = require('cookie-parser');  // クッキーのパースに関するもの
+// クッキーとは「アクセスするそれぞれの人ごとに保管されるデータ」で、サーバーから送られてきた値を保管しておくための仕組み
+var logger = require('morgan');  // httpリクエストのログ出力に関するもの
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); // Add import routes for "catalog" area of site
-const compression = require("compression");
-const helmet = require("helmet");
+const compression = require("compression");  // レスポンスに圧縮を使用する
+const helmet = require("helmet");  // 既知の脆弱性から保護するために使用する
 
 // Create the Express application object
 const app = express();
 
 // Set up rate limiter: maximum of twenty requests per minute
+// APIルートにレート制限を追加する
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
@@ -45,23 +48,29 @@ async function main() {
 }
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));  // テンプレートファイルが保管される場所を設定
+// __dirname: 現在実行中のソースコードが格納されているディレクトリパスが格納されている
+app.set('view engine', 'pug');  // pugをテンプレートエンジンに指定
 
+// モジュールの組み込み
 app.use(logger('dev'));
 app.use(express.json());
+// Body-Parserを基にExpressに組み込まれた機能
+// クライアントから送信されたデータをreq.body経由で取得、操作できる
+// リクエストオブジェクトを JSONオブジェクト として認識する
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression()); // Compress all routes
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));  // イメージ、CSS ファイル、JavaScript ファイルなどの静的ファイルを提供する
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// routing
+app.use('/', indexRouter);  // 「/」にアクセスした場合は「index.js」を実行
+app.use('/users', usersRouter);  // 「/users」にアクセスした場合は、「users.js」を実行
 app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, "Not Found"));
   // same code
   // var err = new Error("Not Found");
   // err.status = 404;
@@ -79,4 +88,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Export the Express application object
 module.exports = app;
